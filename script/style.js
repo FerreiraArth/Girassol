af6597f37e1d45e65e6fbe303f5b441e5d09d8ff
FerreiraArth <arthurferreir4a4@gmail.com>
2023-09-19 18:44:35 -0300

    completed JavaScript form

--- script/style.js
+++ script/style.js
@@ -3,13 +3,21 @@ function carregar(){
     var data = new Date()
     var hora = data.getHours()
 
+    var hora = 15
+
     const imag = document.querySelector('.background')
+
+    const bodyElement = document.body
 
     if (hora >= 5 && hora < 12){
         imag.classList.add('morning')
+        bodyElement.classList.add('bodymorning');
+
     } else if (hora >= 12 && hora < 18 ){
-        imag.classList.add('afternoon')        
+        imag.classList.add('afternoon')  
+        bodyElement.classList.add('bodyafternoon');      
     } else  {
         imag.classList.add('night')   
+        bodyElement.classList.add('bodynight');
     }
 }
