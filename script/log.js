84d5d7cad5c72ce71a6ea2171e854a00db46919f
FerreiraArth <arthurferreir4a4@gmail.com>
2023-09-19 21:15:18 -0300

    Update log.js

--- script/log.js
+++ script/log.js
@@ -2,7 +2,7 @@ function logar(){
     var login = document.getElementById('ilogin').value;
     var senha = document.getElementById('isenha').value;
 
-    if(login == "Girassol" && senha =="Andrade"){
+    if(login == "Girassol" && senha =="Capivara"){
         location.href = "home.html"
     } else {
         alert('Usuário ou senha inexatos')
